8a37996b97e0b47cefd410ddf725721559c10c97
Nicolay Poymanov <n.poymanov@holodilnik.ru>
2024-01-13 14:17:12 +0300

    Добавлен PHP-DI

--- api/public/index.php
+++ api/public/index.php
@@ -10,9 +10,19 @@ http_response_code(500);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = AppFactory::create();
+$builder = new DI\ContainerBuilder();
 
-$app->addErrorMiddleware((bool)getenv('APP_DEBUG'), true, true);
+$builder->addDefinitions([
+    'config' => [
+        'debug' => (bool)getenv('APP_DEBUG'),
+    ],
+]);
+
+$container = $builder->build();
+
+$app = AppFactory::createFromContainer($container);
+
+$app->addErrorMiddleware($container->get('config')['debug'], true, true);
 
 $app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write('{}');
