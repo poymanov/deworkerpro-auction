f7c700dfce8728b29fdd6f37dddff294a712a895
Nicolay Poymanov <n.poymanov@holodilnik.ru>
2024-01-31 21:21:40 +0300

    Добавлен JoinByEmail UseCase

---
+++ api/src/Auth/Entity/User/User.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Auth\Entity\User;
+
+use DateTimeImmutable;
+
+class User
+{
+    private Id $id;
+    private DateTimeImmutable $date;
+    private Email $email;
+    private string $passwordHash;
+    private Status $status;
+    private ?Token $joinConfirmToken;
+
+    public function __construct(
+        Id $id,
+        DateTimeImmutable $date,
+        Email $email,
+        string $passwordHash,
+        Token $token
+    ) {
+        $this->id = $id;
+        $this->date = $date;
+        $this->email = $email;
+        $this->status = Status::wait();
+        $this->passwordHash = $passwordHash;
+        $this->joinConfirmToken = $token;
+    }
+
+    public function isWait(): bool
+    {
+        return $this->status->isWait();
+    }
+
+    public function isActive(): bool
+    {
+        return $this->status->isActive();
+    }
+
+    public function getId(): Id
+    {
+        return $this->id;
+    }
+
+    public function getDate(): DateTimeImmutable
+    {
+        return $this->date;
+    }
+
+    public function getEmail(): Email
+    {
+        return $this->email;
+    }
+
+    public function getPasswordHash(): ?string
+    {
+        return $this->passwordHash;
+    }
+
+    public function getJoinConfirmToken(): ?Token
+    {
+        return $this->joinConfirmToken;
+    }
+}
