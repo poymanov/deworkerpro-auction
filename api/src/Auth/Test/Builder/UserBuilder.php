3e5186f748f67e20cca30362659edaf71ff6bb4d
Nicolay Poymanov <n.poymanov@holodilnik.ru>
2024-02-01 22:24:00 +0300

    Добавлен именованный конструктор для создания пользователя

--- api/src/Auth/Test/Builder/UserBuilder.php
+++ api/src/Auth/Test/Builder/UserBuilder.php
@@ -43,7 +43,7 @@ class UserBuilder
 
     public function build(): User
     {
-        $user = new User(
+        $user = User::requestJoinByEmail(
             $this->id,
             $this->date,
             $this->email,
