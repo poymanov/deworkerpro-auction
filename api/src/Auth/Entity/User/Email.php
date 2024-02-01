69b33b8227ffb71813879506aa08328ba0fdf5f4
Nicolay Poymanov <n.poymanov@holodilnik.ru>
2024-02-01 22:42:53 +0300

    Добавлен запрос на изменение email

--- api/src/Auth/Entity/User/Email.php
+++ api/src/Auth/Entity/User/Email.php
@@ -18,6 +18,11 @@ class Email
         $this->value = mb_strtolower($value);
     }
 
+    public function isEqualTo(self $other): bool
+    {
+        return $this->value === $other->value;
+    }
+
     public function getValue(): string
     {
         return $this->value;
--- api/src/Auth/Test/Builder/UserBuilder.php
+++ api/src/Auth/Test/Builder/UserBuilder.php
@@ -36,6 +36,13 @@ class UserBuilder
         return $clone;
     }
 
+    public function withEmail(Email $email): self
+    {
+        $clone = clone $this;
+        $clone->email = $email;
+        return $clone;
+    }
+
     public function viaNetwork(NetworkIdentity $identity = null): self
     {
         $clone = clone $this;
