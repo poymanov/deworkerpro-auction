4d60a9b69fd721eac4b821fd4886b0cb446e3bcb
Nicolay Poymanov <n.poymanov@holodilnik.ru>
2024-02-01 22:50:20 +0300

    Добавлено удаление пользователя

--- api/src/Auth/Entity/User/User.php
+++ api/src/Auth/Entity/User/User.php
@@ -145,6 +145,13 @@ class User
         $this->role = $role;
     }
 
+    public function remove(): void
+    {
+        if (!$this->isWait()) {
+            throw new DomainException('Unable to remove active user.');
+        }
+    }
+
     public function isWait(): bool
     {
         return $this->status->isWait();
